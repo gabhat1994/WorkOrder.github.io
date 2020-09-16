e25b58a964372e38e3f085716a8c507db4e29d72
stamic1 <bhatgaurav96@gmail.com>
2020-09-16 10:43:06 +0530

    mock api + actions

--- frontend/src/actions/actions.js
+++ frontend/src/actions/actions.js
@@ -1,1 +1,15 @@
 import {} from "../constant/constant"
+
+
+const gridData = (data) => async (dispatch) => {
+  dispatch({ type: "RESTAURANT_LIST_DATA", payload: data });
+};
+
+
+
+ 
+
+
+
+
+export { gridData };
--- frontend/src/compoments/Home.js
+++ frontend/src/compoments/Home.js
@@ -1,12 +1,23 @@
-import React from "react";
-
+import React , {useEffect } from "react";
+import Grid from "./Grid"
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import {gridData} from "../actions/actions"
 export default function Home(props) {
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("./data.json");
+      dispatch(gridData(data.members));
+    };
+    fetchData();
+  }, []);
   return (
     <div
       className="col-md-9"
       style={{ border: "2px solid grey", marginLeft: "15%" }}
     >
+      <Grid />
     </div>
   );
 }
