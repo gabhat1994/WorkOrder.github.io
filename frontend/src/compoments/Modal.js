75c76919b497c68c91106c5fa5392085638477b8
stamic1 <bhatgaurav96@gmail.com>
2020-09-18 11:35:05 +0530

    Date PIcker implementation

--- frontend/src/compoments/Modal.js
+++ frontend/src/compoments/Modal.js
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
-import { Modal , Button } from "antd";
+import { Modal, Button } from "antd";
 import { useSelector } from "react-redux";
+import { DatePicker, Space } from "antd";
+import TimeDisplayGrid from "./TimeDisplayGrid"
+import moment from "moment";
 
-export default function Grid(props) {
+export default function Overlay(props) {
+  const [showData, setShowData] = useState(false);
+  const [date, setDate] = useState("");
   const modalReducer = useSelector((state) => state.modalReducer);
   const { modalData } = modalReducer;
 
+  const dateFormatList = ["MMM-DD-YYYY"];
+
   const handleOk = () => {
     props.modalClose();
+  };
+
+  const onDate = (date, dateString) => {
+    let finalDate = dateString.split("-");
+    let newDateSting = finalDate[0] + " " + finalDate[1] + " " + finalDate[2];
+    setDate(newDateSting)
+    setShowData(true)
+
+
   };
   return (
     <>
@@ -17,15 +33,21 @@ export default function Grid(props) {
         onCancel={handleOk}
         width={1000}
         footer={[
-            <Button key="back" onClick={handleOk}>
-              Close
-            </Button>,
-           
-          ]}
+          <Button key="back" onClick={handleOk}>
+            Close
+          </Button>,
+        ]}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <p>
+          Selet the date for which you want the activity details to be displayed
+        </p>
+        <Space direction="vertical" size={12}>
+          <DatePicker
+            format={dateFormatList}
+            onChange={(date, dateString) => onDate(date, dateString, 1)}
+          />
+        </Space>
+        {showData && <TimeDisplayGrid />}
       </Modal>
     </>
   );
---
+++ frontend/src/compoments/TimeDisplayGrid.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TimeDisplayGrid(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
