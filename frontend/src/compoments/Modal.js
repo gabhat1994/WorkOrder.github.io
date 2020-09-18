9e0da270a4a1aa53f9efc1664aee60f42e8416f6
stamic1 <bhatgaurav96@gmail.com>
2020-09-18 17:43:06 +0530

    code change done

--- frontend/src/compoments/Modal.js
+++ frontend/src/compoments/Modal.js
@@ -1,28 +1,44 @@
 import React, { useState } from "react";
-import { Modal, Button } from "antd";
+import { Modal, Button, Table } from "antd";
 import { useSelector } from "react-redux";
-import { DatePicker, Space } from "antd";
-import TimeDisplayGrid from "./TimeDisplayGrid";
 import moment from "moment";
+import { DatePicker, Space } from "antd";
 
 export default function Overlay(props) {
   const [showData, setShowData] = useState(false);
   const [data, setData] = useState({});
   const modalReducer = useSelector((state) => state.modalReducer);
   const { modalData } = modalReducer;
-
+  const columns = [
+    {
+      title: "Start Time",
+      dataIndex: "start_time",
+      key: "start_time",
+    },
+    {
+      title: "End Time",
+      dataIndex: "end_time",
+      key: "end_time",
+    },
+  ];
   const dateFormatList = ["MMM-DD-YYYY"];
 
   const handleOk = () => {
     props.modalClose();
   };
 
+  const handleReset = () => {
+    setShowData(false);
+    setData({});
+  };
   const onDate = (date, dateString) => {
     let finalDate = dateString.replaceAll("-", " ");
+    let data = [];
     modalData[0].activity_periods.forEach((element) => {
       if (element.start_time.toString().substring(0, 11).trim() == finalDate) {
         let da = element;
-        Promise.resolve(setData(da)).then(function () {
+        data.push(da);
+        Promise.resolve(setData(data)).then(function () {
           setShowData(true);
         });
       }
@@ -49,8 +65,20 @@ export default function Overlay(props) {
             format={dateFormatList}
             onChange={(date, dateString) => onDate(date, dateString, 1)}
           />
+          <Button key="back" onClick={handleReset}>
+            Reset
+          </Button>
+          ,
         </Space>
-        {showData && <TimeDisplayGrid activity={data}/>}
+        <div style={{ marginTop: "30px" }}>
+          {showData == true ? (
+            <Table columns={columns} dataSource={data} pagination={false} />
+          ) : (
+            <div style={{ color: "red" }}>
+              No Data Available for the selected date
+            </div>
+          )}
+        </div>
       </Modal>
     </>
   );
