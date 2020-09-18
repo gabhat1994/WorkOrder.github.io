19e9057cceddb29071b2af5c5eca9817f15fa61a
stamic1 <bhatgaurav96@gmail.com>
2020-09-18 17:54:44 +0530

    Updates

---
+++ precache-manifest.b925ccbaeecabdbf97800a436ad014f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3b9381afa1d9f926d66d918f455d92",
+    "url": "/WorkOrder.github.io/index.html"
+  },
+  {
+    "revision": "69a8e4485addf2673a8c",
+    "url": "/WorkOrder.github.io/static/css/2.0ddb61e8.chunk.css"
+  },
+  {
+    "revision": "9995cf282c3a6cb982e0",
+    "url": "/WorkOrder.github.io/static/css/main.efbad750.chunk.css"
+  },
+  {
+    "revision": "69a8e4485addf2673a8c",
+    "url": "/WorkOrder.github.io/static/js/2.c18b3762.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/WorkOrder.github.io/static/js/2.c18b3762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9995cf282c3a6cb982e0",
+    "url": "/WorkOrder.github.io/static/js/main.f81fd128.chunk.js"
+  },
+  {
+    "revision": "c1455801daf747717475",
+    "url": "/WorkOrder.github.io/static/js/runtime-main.b472d127.js"
+  }
+]);
